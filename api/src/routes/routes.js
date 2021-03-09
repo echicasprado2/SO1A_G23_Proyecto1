be53a6ab6d7ab4bfdf903d9196cd24418301163e
Juan Carlos Aragón <juank1316@gmail.com>
2021-03-08 20:43:12 -0600

    Se realizo primera ruta y configuraciones en server.js

---
+++ api/src/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { json } = require('body-parser')
+
+let router = express.Router()
+
+router.route('/api/data').post((req,res)=>{
+    const data = req.body
+    //Simulate traffic type
+    data.type = 'msqRabbit'
+    
+    res.send('OK')
+})
+
+module.exports = router
--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const port = 3000;
+const routes = require('./routes/routes')
+const bodyParser = require("body-parser")
+const mongoose = require('mongoose')
+
+
 const app = express();
+const mongodbURL = 'mongodb+srv://adminsopes1p1:adminsopes1p1@cluster0.c9orq.mongodb.net/my-db?retryWrites=true&w=majority'
+mongoose.connect(mongodbURL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(routes)
 
 app.listen(port,()=>{
 	console.log(`App escuchando en http://localhost:${port}`)
