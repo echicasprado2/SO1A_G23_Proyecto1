cce17ac45844268b128d16a0295732cf9efec968
Juan Carlos Aragón <juank1316@gmail.com>
2021-03-08 22:27:42 -0600

    Se realizo metodo POST para ingresar datos de casos

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const { json } = require('body-parser')
+const mongoose = require('mongoose')
+const Case = require('../models/case')
 
 let router = express.Router()
 
@@ -7,9 +9,15 @@ router.route('/api/data').post((req,res)=>{
     const data = req.body
     //Simulate traffic type
     data.type = 'msqRabbit'
+    data._id = mongoose.Types.ObjectId()
 
+    console.log(data)
+    let cases = new Case(data)
+    cases.save(function(err,cases){
+        if(err) return console.log("ERORR")
+        console.log("Data Saved")
+    })
 
-    
     res.send('OK')
 })
 
