d8140af561e1aaf38d1cb60a08f79c1dce28de0f
Juan Carlos Aragón <juank1316@gmail.com>
2021-03-09 11:35:30 -0600

    Se creo una funcion para asignar una region a un departamento

---
+++ api/src/utils/region.js
@@ -0,0 +1,52 @@
+
+const getRegion = (location) => {
+    switch(location) {
+        case "Alta Verapaz":
+            return "Verapaz"
+        case "Baja Verapaz":
+            return "Verapaz"
+        case "Chimaltenago":
+            return "Central"
+        case "Chiquimula":
+            return "Nororiente"
+        case "El Progreso":
+            return "Nororiente"
+        case "Escuintla":
+            return "Central"
+        case "Quetzaltenango":
+        case "Guatemala":
+            return "Metropolitana"
+        case "Huehuetenango":
+            return "Noroccidente"
+        case "Izabal":
+            return "Nororiente"
+        case "Jalapa":
+            return "Suroriente"
+        case "Jutiapa":
+            return "Suroriente"
+        case "Petén":
+            return "Petén"
+        case "Quetzaltenango":
+            return "Suroccidente"
+        case "Quiché":
+            return "Noroccidente"
+        case "Retalhuleu":
+            return "Suroccidente"
+        case "Sacatepequez":
+            return "Central"
+        case "San Marcos":
+            return "Suroccidente"
+        case "Santa Rosa":
+            return "Suroriente"
+        case "Sololá":
+            return "Suroccidente"
+        case "Suchitepequez":
+            return "Suroccidente"
+        case "Totonicapán":
+            return "Suroccidente"
+        case "Zacapa":
+            return "Nororiente"
+    }
+}
+
+module.exports = getRegion
