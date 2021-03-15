fd2dc764f41c12094c4d6821d532b473d5fbd7f0
Juan Carlos Aragón <juank1316@gmail.com>
2021-03-15 11:16:05 -0600

    Se cambio el puerto

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const port = 3000;
+const port = 3001;
 const routes = require('./routes/routes')
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
