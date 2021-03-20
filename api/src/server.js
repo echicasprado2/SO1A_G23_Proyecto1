625bcae03dc1a65ae164682a4726e3e06d0dfe2a
EverChicas <2373708320101@ingenieria.usac.edu.gt>
2021-03-20 12:31:04 -0600

    added nodejs connection with mongodb

--- api/src/server.js
+++ api/src/server.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose')
 
 
 const app = express();
-const mongodbURL = 'mongodb+srv://adminsopes1p1:adminsopes1p1@cluster0.c9orq.mongodb.net/my-db?retryWrites=true&w=majority'
+// const mongodbURL = 'mongodb+srv://adminsopes1p1:adminsopes1p1@cluster0.c9orq.mongodb.net/my-db?retryWrites=true&w=majority'
+const mongodbURL = 'mongodb://localhost:27017/sopes1'
 mongoose.connect(mongodbURL, {useNewUrlParser: true, useUnifiedTopology: true});
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(routes)
