e321c8a6b82268a04423984acc1e8947b86352db
Josue <Joshua72.perez@gmail.com>
2021-03-25 09:42:40 -0600

    ruta publisher

--- mensajeria/google_pubsub/publisher/publisher.go
+++ mensajeria/google_pubsub/publisher/publisher.go
@@ -71,7 +71,7 @@ func publicar(mensaje string) error {
 func http_server(w http.ResponseWriter, r *http.Request) {
 	r.Header.Set("Accept", "application/json")
 	// Comprobamos que el path sea exactamente '/' sin parámetros
-    if r.URL.Path != "/pubsub" {
+    if r.URL.Path != "/" {
         http.Error(w, "404 not found.", http.StatusNotFound)
         return
     }
